72b514e2d4ef71a241fda2b1b507236b77bb5310
DevDengChao <2325690622@qq.com>
2022-08-30 21:34:21 +0800

    build: setup cypress for e2e tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "cypress";
+import {defineConfig} from "cypress";
 
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
-  component: {
-    devServer: {
-      framework: "vue",
-      bundler: "vite",
+    component: {
+        devServer: {
+            framework: "vue",
+            bundler: "vite",
+        },
     },
-  },
+    e2e: {
+        baseUrl: "http://localhost:4173",
+    }
 });
