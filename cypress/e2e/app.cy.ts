7caf902a192b1fdad3027aa843fe59d79d669c9a
DevDengChao <2325690622@qq.com>
2022-08-30 23:02:29 +0800

    refactor: organizing cypress test cases

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,17 @@
-describe('empty spec', () => {
-    it('passes', () => {
-        cy.visit('/')
-        cy.contains("h1", "Vite + Vue");
+describe('when visiting /', function () {
+    let page: Cypress.Chainable<Cypress.AUTWindow>;
+
+    beforeEach(() => {
+        page = cy.visit('/');
     })
-})
+
+    it("then there is an h1 contains 'Vite + Vue'", () => {
+        page.contains("h1", "Vite + Vue");
+    })
+
+    it("and clicking buttons, then button inner text changed", () => {
+        page.find("[data-testid='btn']").click().should('have.text', 'count is 1');
+        page.find("[data-testid='element-plus-btn']").click().should('have.text', 'count is 1');
+        page.find("[data-testid='element-plus-global-btn']").click().should('have.text', 'count is 1');
+    })
+});
